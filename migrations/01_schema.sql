7fc436aed2a7d94f093f84fa88c15296918c0b7d
saintsappho <robinrosefleur@gmail.com>
2023-12-13 19:22:07 -0700

    retroactively adding schema and seeds. big oops.

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -20,8 +20,8 @@ CREATE TABLE properties (
   cover_photo_url VARCHAR(255) NOT NULL,
   cost_per_night INTEGER NOT NULL DEFAULT 0,
   parking_spaces INTEGER NOT NULL DEFAULT 0,
-  num_bathrooms INTEGER NOT NULL DEFAULT 0,
-  num_bedrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
   -- 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
